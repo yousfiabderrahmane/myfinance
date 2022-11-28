ba66456786014f0f3a79e7f828f399d869c4dbd3
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-11-28 11:59:39 +0100

    main: conditioanall showing user content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,27 +3,37 @@ import styles from "./Navbar.module.css";
 //
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Navbar() {
   const { logout } = useLogout();
+
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myFinance</li>
 
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-
-        <li>
-          <button className="btn" onClick={logout}>
-            Logout
-          </button>
-        </li>
+        {!user ? (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>Hello, {user.displayName}!</li>
+            <li>
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
