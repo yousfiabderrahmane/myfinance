6c50d14380400b0fef0dcf8a0af4817db60380b6
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-11-28 09:16:13 +0100

    main : firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDj463xxp7XegX_Ia_JBUGdT_NGoOeU0kg",
+  authDomain: "myfinance-807ed.firebaseapp.com",
+  projectId: "myfinance-807ed",
+  storageBucket: "myfinance-807ed.appspot.com",
+  messagingSenderId: "453682310227",
+  appId: "1:453682310227:web:48aa097b179965b4d9aaa6",
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init service
+
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
