682173202d714c59cd67b71e0ca305ed9b2add4f
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-11-28 10:58:58 +0100

    main: using the logout hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,11 @@
 import styles from "./Navbar.module.css";
 //
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 export default function Navbar() {
+  const { logout } = useLogout();
+
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -14,6 +17,12 @@ export default function Navbar() {
         </li>
         <li>
           <Link to="/signup">Signup</Link>
+        </li>
+
+        <li>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
         </li>
       </ul>
     </nav>
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import projectAuth from "../firebase/config";
-import useAuthContext from "./useAuthContext";
+import { projectAuth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
 
 export const useLogout = () => {
   const [error, setError] = useState(null);
