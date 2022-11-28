38d4ece8cdc73f3ab7c17b72faabdcb5da1dc1af
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-11-28 09:39:20 +0100

    main : creating a signup hook

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { projectAuth } from "../firebase/config";
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  const signup = async (email, password, displayName) => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      //signup user
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(res.user); //user is a proprety on the res object
+
+      if (!res) {
+        throw new Error("Could not complete signup");
+      }
+
+      // add display name to user by updating profile
+      // {displayName: displayName} they match so we can shorten it
+      await res.user.updateProfile({ displayName });
+
+      setIsPending(false);
+      setError(null);
+    } catch (error) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
+
+  return { error, isPending, signup };
+};
