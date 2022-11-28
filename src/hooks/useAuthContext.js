fcaed51d32a6ecfb454e32d1b2493fbef44a4d56
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-11-28 10:12:24 +0100

    main: creating an auth context

---
+++ src/hooks/useAuthContext.js
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+
+  //check if used out of its scoop
+  if (!context) {
+    throw Error("useAuthContext must be inside an AuthCOntextProvider");
+  }
+
+  return context;
+};
